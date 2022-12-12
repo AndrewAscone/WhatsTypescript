e0d8006847755f8c4b3121c4d6aa9f7acac509f7
AndrewAscone <andrew.ascone@gmail.com>
2022-12-11 23:41:07 -0500

    Able to pull info from API and print to webpage

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
-"use strict";
-exports.__esModule = true;
-// Import stylesheets
-require("./style.css");
+// // Import stylesheets
+// import './style.css';
 var form = document.querySelector('#defineform');
-form.onsubmit = function () {
-    var formData = new FormData(form);
-    console.log(formData);
-    var text = formData.get('defineword');
-    console.log(text);
-    return false; // prevent reload
-};
+if (form != null) {
+    form.onsubmit = function () {
+        var formData = new FormData(form);
+        var text = formData.get('defineword');
+        var apiURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + text;
+        fetch(apiURL)
+            .then(function (response) {
+            return response.json();
+        })
+            .then(function (data) {
+            var oldList = document.querySelector('.list-group');
+            var listParent = document.createElement('ul');
+            listParent.classList.add('list-group');
+            var firstList = document.createElement('li');
+            firstList.classList.add('list-group-item');
+            firstList.textContent = data[0].word;
+            listParent.append(firstList);
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < data[i].meanings.length; j++) {
+                    var partOfSpeechItem = document.createElement('li');
+                    partOfSpeechItem.textContent = "Parts of speech: " + data[i].meanings[j].partOfSpeech;
+                    var definitionTitle = document.createElement('li');
+                    definitionTitle.textContent = 'Definition: ';
+                    var definitionItemList = document.createElement('ul');
+                    definitionTitle.append(definitionItemList);
+                    for (var k = 0; k < data[i].meanings[j].definitions.length; k++) {
+                        var definitionItem = document.createElement('li');
+                        definitionItem.textContent = data[i].meanings[j].definitions[k].definition;
+                        definitionItemList.append(definitionItem);
+                    }
+                    listParent.append(partOfSpeechItem, definitionTitle);
+                }
+            }
+            oldList === null || oldList === void 0 ? void 0 : oldList.replaceWith(listParent);
+            console.log(data);
+        })["catch"](function (error) { return console.log(error); });
+        console.log(text);
+        return false; // prevent reload
+    };
+}
+// async function getDefinition(){
+//   const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/dog');
+//   const data = await response.json();
+//   console.log(data[0].word);
+//   console.log(data[0].phonetic);
+//   console.log(data[0].meanings[0].partOfSpeech)
+//   console.log(data[0].meanings[0].definitions[0].definition)
+// }
+// getDefinition();
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-// Import stylesheets
-import './style.css';
+// // Import stylesheets
+// import './style.css';
 
 
 const form: HTMLFormElement = document.querySelector('#defineform');
@@ -9,17 +9,62 @@ if(form != null){
     const formData = new FormData(form);
 
     const text = formData.get('defineword') as string;
-    const requestURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + text;
+    const apiURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + text;
+
+    fetch(apiURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const oldList =document.querySelector('.list-group');
+
+      const listParent: HTMLUListElement = document.createElement('ul');
+      listParent.classList.add('list-group');
+
+      const firstList: HTMLLIElement = document.createElement('li');
+      firstList.classList.add('list-group-item');
+      firstList.textContent = data[0].word;
+      listParent.append(firstList);
+
+      for(let i = 0; i < data.length; i++){
+        for(let j = 0; j < data[i].meanings.length; j++){
+          const partOfSpeechItem: HTMLLIElement = document.createElement('li');
+          partOfSpeechItem.textContent = "Parts of speech: " + data[i].meanings[j].partOfSpeech;
+
+          const definitionTitle: HTMLLIElement = document.createElement('li');
+          definitionTitle.textContent = 'Definition: ';
+
+          const definitionItemList: HTMLUListElement = document.createElement('ul');
+          definitionTitle.append(definitionItemList);
+
+          for(let k = 0; k < data[i].meanings[j].definitions.length; k++){
+            const definitionItem: HTMLLIElement = document.createElement('li');
+            definitionItem.textContent = data[i].meanings[j].definitions[k].definition;
+            definitionItemList.append(definitionItem);
+          }
+
+          listParent.append(partOfSpeechItem, definitionTitle);
+        }
+      }
+
+      oldList?.replaceWith(listParent);
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+
+    console.log(text);
+    return false; // prevent reload
   }
 }
 
+// async function getDefinition(){
+//   const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/dog');
+//   const data = await response.json();
+  
+//   console.log(data[0].word);
+//   console.log(data[0].phonetic);
+//   console.log(data[0].meanings[0].partOfSpeech)
+//   console.log(data[0].meanings[0].definitions[0].definition)
+// }
 
-
-form.onsubmit = () => {
-  const formData = new FormData(form);
-
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  console.log(text);
-  return false; // prevent reload
-};
+// getDefinition();
