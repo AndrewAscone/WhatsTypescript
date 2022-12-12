c697a6f589f571217e9c37917982d76a58ac569e
AndrewAscone <andrew.ascone@gmail.com>
2022-12-11 21:17:01 -0500

    Figured out parsing individual JSON pieces from word URL

--- index.ts
+++ index.ts
@@ -4,6 +4,16 @@ import './style.css';
 
 const form: HTMLFormElement = document.querySelector('#defineform');
 
+if(form != null){
+  form.onsubmit = () =>{
+    const formData = new FormData(form);
+
+    const text = formData.get('defineword') as string;
+    const requestURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + text;
+  }
+}
+
+
 
 form.onsubmit = () => {
   const formData = new FormData(form);
